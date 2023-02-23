c19d47afa8c5e5032b737d72b64d09ed8106f9b2
renato-albuquerque <renatomalb83@gmail.com>
2023-02-23 20:15:22 -0300

    added buttons: dark/light mode and share

---
+++ index.js
@@ -0,0 +1,37 @@
+// VARIABLES
+
+const buttonDarkMode = document.querySelector('.darkMode')
+const buttonLightMode = document.querySelector('.lightMode')
+const buttonShare = document.querySelector('.share')
+const html = document.querySelector('html')
+
+
+// EVENTS
+
+buttonLightMode.addEventListener('click', function() {
+    html.classList.add('lightMode')
+    buttonLightMode.classList.add('iconTurnsColor')
+    buttonDarkMode.classList.add('iconTurnsColor')
+    buttonShare.classList.add('iconTurnsColor')
+})
+
+buttonDarkMode.addEventListener('click', function() {
+    html.classList.remove('lightMode')
+    buttonLightMode.classList.remove('iconTurnsColor')
+    buttonDarkMode.classList.remove('iconTurnsColor')
+    buttonShare.classList.remove('iconTurnsColor')
+})
+
+// FUNCTION
+
+function share(){
+	if (navigator.share !== undefined) {
+		navigator.share({
+			title: 'Cartão de Visita Digital - Rivana Albuquerque',
+			text: 'Social Media',
+			url: 'https://cartaodevisita-rivana.vercel.app/'
+		})
+		.then(() => console.log('Successful share'))
+		.catch((error) => console.log('Error sharing', error));
+	}
+}
